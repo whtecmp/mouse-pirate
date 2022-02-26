a60710f821457aeb16a855728180e26c47aaa6ad
Your Name <you@example.com>
2022-02-26 16:57:47 +0200

    Added attack mode

--- Player.gd
+++ Player.gd
@@ -18,6 +18,10 @@ var input_to_function = {
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var can_idle = true;
+var return_attack = false;
+var is_attacking = false;
+var is_fliped = false;
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -31,21 +35,39 @@ func _physics_process(delta):
 		motion_velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("a") and is_on_floor():
 		motion_velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("s"):
+		$AnimatedSprite.play("Attack")
+		can_idle = false;
+		is_attacking = true;
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction and not is_attacking:
 		motion_velocity.x = direction * SPEED
 		$AnimatedSprite.play("Run")
 		if direction < 0:
-			$AnimatedSprite.flip_h = true;
+			is_fliped = true;
 		else:
-			$AnimatedSprite.play("Run")
-			$AnimatedSprite.flip_h = false;
+			is_fliped = false;
 	else:
 		motion_velocity.x = move_toward(motion_velocity.x, 0, SPEED)
-		$AnimatedSprite.play("Idle")
+		if can_idle:
+			$AnimatedSprite.play("Idle") 
+	$AnimatedSprite.flip_h = is_fliped;
 	move_and_slide()
+
+func _on_animated_sprite_animation_finished():
+	if $AnimatedSprite.animation == "Attack":
+		if return_attack:
+			$AnimatedSprite.play("Idle")
+			can_idle = true;
+			return_attack = false;
+			is_attacking = false;
+		else:
+			$AnimatedSprite.stop()
+			$AnimatedSprite.play("Attack", true)
+			return_attack = true;
