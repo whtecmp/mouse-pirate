3bdd3ac9fa55782a239baa6fea4d6c755b45bc14
Your Name <you@example.com>
2022-03-11 19:46:37 +0200

    Mob following and attacking

--- enemy_mouse.gd
+++ enemy_mouse.gd
@@ -7,11 +7,19 @@ func flip(to_flip):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var _self = self;
-	$Mob.play_animation = func(anim):
-		_self.get_node("Mob/MouseBody").play(anim);
-		_self.get_node("Mob/MouseWeaponHand").play(anim);
+	$Mob.stop_animation = func():
+		_self.get_node("Mob/MouseBody").stop();
+		_self.get_node("Mob/MouseWeaponHand").stop();
+	$Mob.play_animation = func(anim, backwards = false):
+		_self.get_node("Mob/MouseBody").play(anim, backwards);
+		_self.get_node("Mob/MouseWeaponHand").play(anim, backwards);
 	$Mob.flip = flip;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_mouse_body_animation_finished():
+	if $Mob/MouseBody.animation == "Attack":
+		$Mob.attack_finished();
--- mob.gd
+++ mob.gd
@@ -3,12 +3,17 @@ extends CharacterBody2D
 const SPEED = 250.0
 const JUMP_VELOCITY = -400.0
 
-enum {TOO_CLOSE, TOO_FAR, LEFT, RIGHT }
+enum {TOO_CLOSE_LEFT, TOO_CLOSE_RIGHT, TOO_FAR, LEFT, RIGHT }
 
 var wants_jump = false;
 var wants_attack = false;
-var walk_direction = 0;
+var is_attacking = false;
+var return_attack = false;
+var can_idle = true;
+var walk = false;
+var direction = 0;
 var play_animation;
+var stop_animation;
 var flip;
 
 
@@ -21,8 +26,10 @@ func assess_relative_position_to_objective(objective, min_distance_threshold, ma
 		return LEFT;
 	elif -min_distance_threshold > distance_from_object and distance_from_object > -max_distance_threshold:
 		return RIGHT;
-	elif -min_distance_threshold < distance_from_object and distance_from_object < min_distance_threshold:
-		return TOO_CLOSE;
+	elif -min_distance_threshold < distance_from_object and distance_from_object < 0:
+		return TOO_CLOSE_LEFT;
+	elif 0 < distance_from_object and distance_from_object < min_distance_threshold:
+		return TOO_CLOSE_RIGHT;
 	else:
 		return TOO_FAR;
 
@@ -31,34 +38,60 @@ func _physics_process(delta):
 	if not is_on_floor():
 		motion_velocity.y += gravity * delta
 	
-	var relative_position_to_objective = assess_relative_position_to_objective(get_tree().get_root().get_node("Main/Player"), 150, 300);
-	{
+	var relative_position_to_objective = assess_relative_position_to_objective(get_tree().get_root().get_node("Main/Player"), 60, 500);
+	{ # switch case relative_position_to_objective
 		LEFT: func (_self):
-			_self.walk_direction = -1;,
+			_self.walk = true;
+			_self.direction = -1,
 		RIGHT: func (_self):
-			_self.walk_direction = 1;,
-		TOO_CLOSE: func (_self):
-			_self.walk_direction = 0;,
+			_self.walk = true;
+			_self.direction = 1,
 		TOO_FAR: func (_self):
-			_self.walk_direction = 0;
+			_self.walk = false;,
+		TOO_CLOSE_LEFT: func (_self):
+			_self.direction = -1;
+			_self.walk = false;
+			_self.wants_attack = true;,
+		TOO_CLOSE_RIGHT: func (_self):
+			_self.direction = 1;
+			_self.walk = false;
 			_self.wants_attack = true;
 	}[relative_position_to_objective].call(self);
 
+	if wants_attack and not is_attacking:
+		wants_attack = false;
+		play_animation.call("Attack");
+		is_attacking = true;
+		can_idle = false;
+
 	# Handle Jump.
-	if wants_jump and is_on_floor():
+	if wants_jump and is_on_floor() and not is_attacking:
 		motion_velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	if walk_direction:
-		motion_velocity.x = walk_direction * SPEED
+	print (can_idle)
+	if walk and not is_attacking:
+		motion_velocity.x = direction * SPEED
 		play_animation.call("Run")
 	else:
-		play_animation.call("Idle")
+		if can_idle:
+			play_animation.call("Idle")
 		motion_velocity.x = move_toward(motion_velocity.x, 0, SPEED)
-	if walk_direction > 0:
+	if walk and not is_attacking and direction > 0:
 		flip.call(false);
-	elif walk_direction < 0:
+	elif walk and not is_attacking and direction < 0:
 		flip.call(true);
 
 	move_and_slide()
+
+func attack_finished():
+	if return_attack:
+		play_animation.call("Idle")
+		can_idle = true;
+		return_attack = false;
+		is_attacking = false;
+	else:
+		stop_animation.call()
+		play_animation.call("Attack", true)
+		return_attack = true;
